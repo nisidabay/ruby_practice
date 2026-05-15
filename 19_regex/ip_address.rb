f569ba8fadb952520c6013a40a11bd18848fef1f
nisidabay <nisidabay@gmail.com>
2026-05-15 06:20:53 +0200

    feat: modify 19_regex/ip_address.rb

--- 19_regex/ip_address.rb
+++ 19_regex/ip_address.rb
@@ -2,9 +2,8 @@
 #
 # !! Convert the return value to boolean
 def ip_address?(str)
-  !!(str =~ 
-/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
+  !!(str =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
 end
 
-p ip_address?("192.168.1.1")
-p ip_address?("192.168.1")
+p ip_address?('192.168.1.1')
+p ip_address?('192.168.1')
